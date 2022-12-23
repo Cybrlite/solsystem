a20cf133d8bfd9a23a9840491e3e67de3be5c538
star-scope <caleb.faulkner@icloud.com>
2022-12-22 22:09:23 -0500

    jquery menu working

--- menu.js
+++ menu.js
@@ -1,31 +1,17 @@
-document.getElementById("menuButton").addEventListener("click", clickMenuButton)
-document.getElementById("menuIcon").addEventListener("click", clickMenuIcon)
-
-function clickMenuButton(e){
-  let node = e.target;
-  document.getElementById("menu").style.display = "block"
-}
-
-function clickMenuIcon(e){
-  let node = e.target;
-  document.getElementById("menu").style.display = "none"
-}
-
-window.addEventListener("click", function(e) {
-  let node = e.target;
-  const menuElement = document.getElementById("menu");
-  const cssObj = window.getComputedStyle(menuElement, null);
-  let arrParents = getParents(node);
-  if ((node.id != "menu" && node.id != "menuButton") && (!arrParents.includes("menu") && !arrParents.includes("menuButton")) && cssObj.getPropertyValue("display") != "none") {
-    document.getElementById("menu").style.display = "none"
-  }
+$(function() {
+  $('#menuButton').on('click', function(e) {
+    $('#menu').show();
+  });
 });
 
-function getParents(elem) {
-  let parents = [];
-  while(elem.parentNode && elem.parentNode.nodeName.toLowerCase() != "body") {
-    elem = elem.parentNode;
-    parents.push(elem.id);
+$(function() {     
+  $('#menuIcon').on('click', function(e) {
+    $('#menu').hide();
+  });
+});
+
+$('html').click(function(event) {
+  if ($(event.target).closest('#menu').length === 0) {
+      $('#menu').hide();
   }
-  return parents;
-}
+});
