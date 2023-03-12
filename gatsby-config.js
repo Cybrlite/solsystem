a1c386d9a0510c5b844d8b2ba9eb019fdbeee5ec
calebddawson <calebdawsondev@gmail.com>
2023-03-11 22:35:31 -0500

    Angled quotes to standard quotes

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,22 +9,22 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Sol System`,
-    description: `Interactive solar system explorer.`,
-    author: `Cybrlite`,
-    siteUrl: `https://solsystem.space`,
+    title: 'Sol System',
+    description: 'Interactive solar system explorer.',
+    author: 'Cybrlite',
+    siteUrl: 'https://solsystem.space',
   },
   plugins: [
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        display: `minimal-ui`,
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        display: 'minimal-ui',
         icon: 'public/images/logo.png'
       },
     },
